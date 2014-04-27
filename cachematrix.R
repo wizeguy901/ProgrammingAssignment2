1a36358e01de11f44871a8b799328db7832baeec
wizeguy901 <ilyavolf@yahoo.com>
2014-04-27 16:43:30 -0400

    final version of hwk2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## MakeCacheMatrix is a function that returns a "special" object, a matrix object
+## that is cacheable 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y){
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse = matrix()) s <<- inverse
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## solves a matrix, and sets the "s" property of our newly created object to the
+## inverse, thus caching it for possible use in the future
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinverse(s)
+  s
 }
